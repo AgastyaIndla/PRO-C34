633b77b0711d1c63932015d615df26579242d4f3
AgastyaIndla <63347849+AgastyaIndla@users.noreply.github.com>
2020-05-29 09:33:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,47 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+var database;
+
+var drawing = [];
+var currentPath = [];
+
+function setup(){
+  canvas = createCanvas(500,500);
+  canvas.mousePressed(startPath);
+  //canvas.mouseReleased(endPath);
+ 
 }
 
+function startPath() {
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+//function endPath() {
+ // drawing.push(currentPath);
+//}
+
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(0,100);
+  
+  if(mouseIsPressed) {
+    var point = {
+     x: mouseX,
+     y: mouseY 
+    }
+
+    currentPath.push(point);
+  }
+
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+
+  for (var i = 0; i < drawing.length; i++ ) {
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++) {
+    vertex(path[j].x, path[j].y );
+  }
+   endShape();
+  }
+
 }
